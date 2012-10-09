e78db776839b5f2dc2dabaa780d8abf3a230f980
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2012-10-09 21:43:51 +0200

    Clean up the userspace program, more work on the presentation.

--- userspace/src/aaa_add.c
+++ userspace/src/aaa_add.c
@@ -7,37 +7,42 @@
 
 #include "faaio.h"
 
+void add(int, uint32_t, uint32_t);
+
 static const char* faa_device = "/dev/faa0";
 
 int
 main(int argc, char *argv[])
 {
 	int devfd;
-	struct faaio_add faaio;
-	uint32_t result;
 
 	if (argc != 3) {
 		printf("usage: %s a b\n", argv[0]);
 		return 1;
 	}
-
-	faaio.result = &result;
-	faaio.a = atoi(argv[1]);
-	faaio.b = atoi(argv[2]);	
-
 	if ( (devfd = open(faa_device, O_RDWR)) == -1) {
 		perror("can't open device file");
 		return 1;
 	}
 
-	if (ioctl(devfd, FAAIO_ADD, &faaio) == -1) {
-		perror("ioctl failed");
-		return 1;
-	}
+	add(devfd, atoi(argv[1]), atoi(argv[2]));
 
-	printf("%d\n", result);
 	close(devfd); 
-
 	return 0;
 }
 
+void
+add(int devfd, uint32_t a, uint32_t b)
+{
+	struct faaio_add faaio;
+	uint32_t result = 0;
+
+	faaio.result = &result;
+	faaio.a = a;
+	faaio.b = b;
+
+	if (ioctl(devfd, FAAIO_ADD, &faaio) == -1) {
+		perror("ioctl failed");
+	}
+	printf("%d\n", result);
+}
